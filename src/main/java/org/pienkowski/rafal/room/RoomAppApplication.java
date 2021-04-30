5660f659005abece1dd185607ece7b27d74ffa3e
Rafal <rafal.pienkowski.mail@gmail.com>
2021-04-30 09:37:12 +0200

    Initial commit - Spring Boot app

---
+++ src/main/java/org/pienkowski/rafal/room/RoomAppApplication.java
@@ -0,0 +1,13 @@
+package org.pienkowski.rafal.room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pienkowski/rafal/room/RoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pienkowski.rafal.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
